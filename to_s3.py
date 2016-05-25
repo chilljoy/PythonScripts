61093ad314d1e8357b2c6842687187d261bef592
chilljoy <chilljoy@mac.com>
2016-05-25 15:32:00 -0500

    Delete to_s3.py

--- to_s3.py
+++
@@ -1,7 +0,0 @@
-import os
-
-source = raw_input('Enter source item name, case sensitve: ')
-
-dest = raw_input('Enter dest folder name, case sensitve: ')
-
-os.system("s3cmd put "+ source + " s3://employeebackups/" + dest + "/ -vv --multipart-chunk-size-mb=200")
